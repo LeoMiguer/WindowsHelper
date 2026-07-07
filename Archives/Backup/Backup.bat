e464a58b16de6c16a4e1655e5d8f118a90ecf922
Leonardo Miguel Machado Souza <leomiguelmachado@gmail.com>
2026-07-07 12:07:34 -0300

    Design Windows Helper - Release Version (v1.3)
    A Windows Helper developed for Design Cursos

--- Archives/Backup/Backup.bat
+++ Archives/Backup/Backup.bat
@@ -1,18 +1,10 @@
 @echo off
 echo -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-echo Pressione qualquer tecla para iniciar a criacao do Backup.
+echo ...Em breve sera implementado a funcao de criar backup...
 echo -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-echo O Programa vai criar um ponto de restauracao
-echo -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-echo CERTIFIQUE-SE DE QUE TODAS AS OPERACOES FORAM FINALIZADAS!!!
-timeout 3
-color 0c
-pause
 
-powershell -ExecutionPolicy Bypass -Command "Enable-ComputerRestore -Drive 'C:\'"
+timeout 5
 
-powershell -ExecutionPolicy Bypass -Command "Checkpoint-Computer -Description 'DesignBackup' -RestorePointType MODIFY_SETTINGS"
-
-start rstrui.exe
+::start rstrui.exe
 
 exit
